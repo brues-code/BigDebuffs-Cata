0b9ac41a23ae631e0e70d1cc6c192a2a5a10fea5
Skyler H <skylerh15@users.noreply.github.com>
2021-12-10 22:16:48 -0600

    spell id updates
    Thanks Handra!

--- BigDebuffs.lua
+++ BigDebuffs.lua
@@ -90,9 +90,11 @@ BigDebuffs.Spells = {
 	[29166] = { type = "buffs_offensive", },  -- Innervate
 	[50334] = { type = "buffs_offensive", },  -- Berserk
 	[69369] = { type = "buffs_offensive", }, -- Predator's Swiftness
-	[53201] = { type = "buffs_offensive", }, -- Starfall
-	[53312] = { type = "buffs_other", }, -- Nature's Grasp
-	[33357] = { type = "buffs_other", },  -- Dash
+	[77761] = { type = "buffs_other" }, -- Stampeding Roar (Bear)
+	[77764] = { type = "buffs_other" }, -- Stampeding Roar (Cat)
+	[48505] = { type = "buffs_offensive", }, -- Starfall
+	[16689] = { type = "buffs_other", }, -- Nature's Grasp
+	[1850] = { type = "buffs_other", },  -- Dash
 	[768] = { type = "buffs_other", }, -- Cat Form
 	[5487] = { type = "buffs_other", }, -- Bear Form
 	[783] = { type = "buffs_other", }, -- Travel Form
@@ -101,10 +103,10 @@ BigDebuffs.Spells = {
 	[22570] = { type = "cc" },  -- Maim
 	[5211] = { type = "cc", },  -- Bash
 	[2637] = {type = "cc"}, -- Hibernate
-	[49803] = { type = "cc", },  -- Pounce
+	[9005] = { type = "cc", },  -- Pounce
 	[33786] = { type = "immunities" },  -- Cyclone
 	[45334] = { type = "roots", },  -- Feral Charge Effect (Immobilize)
-	[53308] = { type = "roots", },  -- Entangling Roots
+	[339] = { type = "roots", },  -- Entangling Roots
 	[53313] = { type = "roots", }, -- Entangling Roots (From Nature's Grasp)
 	[45334] = { type = "interrupts", interruptduration = 4, },  -- Feral Charge Effect (Interrupt)
 	-- Hunter
@@ -120,32 +122,30 @@ BigDebuffs.Spells = {
 	[19574] = { type = "immunities", }, -- Bestial Wrath (Pet)
 	[24394] = { type = "cc", },  -- Intimidation (Stun)
 		[19577] = { type = "buffs_offensive", }, -- Intimidation (Pet Buff)
-	[49012] = { type = "cc", },  -- Wyvern Sting
+	[19386] = { type = "cc", },  -- Wyvern Sting
 	[19503] = { type = "cc", },  -- Scatter Shot
 	[3355] = { type = "cc", },  -- Freezing Trap
-	[14327] = { type = "cc", }, -- Scare Beast
+	[1513] = { type = "cc", }, -- Scare Beast
 	[35101] = { type = "cc", }, -- Chimera Shot - Daze
-	[53562] = { type = "cc", }, -- Ravage (Pet)
-	[53543] = { type = "cc", }, -- Snatch (Pet Disarm)
+	[50518] = { type = "cc", }, -- Ravage (Pet)
+	[91644] = { type = "cc", }, -- Snatch (Pet Disarm)
 	[34490] = { type = "cc", }, -- Silencing Shot
 	[19306] = { type = "roots", }, -- Counterattack
 	[19185] = { type = "roots", }, -- Entrapment
 		[64803] = { type = "roots", },
-		[64804] = { type = "roots", },
-	[53548] = { type = "roots", }, -- Pin (Pet)
+		[19184] = { type = "roots", },
+	[50245] = { type = "roots", }, -- Pin (Pet)
 	[4167] = { type = "roots", }, -- Web (Pet)
 	[26090] = { type = "interrupts", interruptduration = 2, }, -- Pummel (Pet)
 	-- Mage
 	[11426] = { type = "buffs_other", },  -- Ice Barrier
 	[12472] = { type = "buffs_offensive", },  -- Icy Veins
-	[54748] = { type = "buffs_offensive", }, -- Burning Determination (Interrupt/Silence Immunity)
 	[12042] = { type = "buffs_offensive", },  -- Arcane Power
 	[12043] = { type = "buffs_offensive", },  -- Presence of Mind
 	[12051] = { type = "buffs_offensive", },  -- Evocation
 	[44544] = { type = "buffs_offensive", }, -- Fingers of Frost
 	[66] = { type = "buffs_offensive", },  -- Invisibility
 	[118] = { type = "cc", },  -- Polymorph
-		[12826] = { type = "cc", },
 		[71319] = { type = "cc", },
 		[28271] = { type = "cc", },
 		[28272] = { type = "cc", },
@@ -157,7 +157,6 @@ BigDebuffs.Spells = {
 	[55021] = { type = "cc", }, -- Improved Counterspell
 	[64346] = { type = "cc", }, -- Fiery Payback (Fire Mage Disarm)
 	[45438] = { type = "immunities", },  -- Ice Block
-	[12494] = { type = "roots", },  -- Frostbite
 	[122] = { type = "roots", },  -- Frost Nova
 	[2139] = { type = "interrupts", interruptduration = 6, },  -- Counterspell (Mage)
 	-- Paladin
@@ -165,7 +164,7 @@ BigDebuffs.Spells = {
 	[96263] = { type = "buffs_other", }, -- Sacred Shield Proc
 	[59578] = { type = "buffs_other", }, -- The Art of War
 	[1022] = { type = "buffs_defensive", }, -- Hand of Protection
-	[31852] = { type = "buffs_defensive", },  -- Ardent Defender
+	[31850] = { type = "buffs_defensive", },  -- Ardent Defender
 	[31821] = { type = "buffs_defensive", },  -- Aura Mastery
 	[498] = { type = "buffs_defensive", },  -- Divine Protection
 	[6940] = { type = "buffs_defensive", },  -- Hand of Sacrifice
@@ -189,10 +188,10 @@ BigDebuffs.Spells = {
 	[64901] = { type = "buffs_defensive", }, -- Hymn of Hope
 	[10060] = { type = "buffs_offensive", },  -- Power Infusion
 	[6346] = { type = "buffs_other", },  -- Fear Ward
-	[48066] = { type = "buffs_other", }, -- Power Word: Shield
+	[17] = { type = "buffs_other", }, -- Power Word: Shield
 	[64044] = { type = "cc", }, -- Psychic Horror (Horrify)
 	[64058] = { type = "cc", }, -- Psychic Horror (Disarm)
-	[10890] = { type = "cc", },  -- Psychic Scream
+	[8122] = { type = "cc", },  -- Psychic Scream
 	[15487] = { type = "cc", },  -- Silence
 	[605] = { type = "cc", },  -- Mind Control
 	[9484] = { type = "cc", },  -- Shackle Undead
@@ -202,12 +201,12 @@ BigDebuffs.Spells = {
 	[13750] = { type = "buffs_offensive", },  -- Adrenaline Rush
 	[51690] = { type = "buffs_offensive", },  -- Killing Spree
 	[14177] = { type = "buffs_offensive", }, -- Cold Blood
-	[11305] = { type = "buffs_other", },  -- Sprint
-	[26669] = { type = "buffs_defensive", },  -- Evasion
+	[2983] = { type = "buffs_other", },  -- Sprint
+	[5277] = { type = "buffs_defensive", },  -- Evasion
 	[1776] = { type = "cc", },  -- Gouge
 	[51722] = {type = "cc", }, -- Dismantle
 	[2094] = { type = "cc", },  -- Blind
-	[8643] = { type = "cc", },  -- Kidney Shot
+	[408] = { type = "cc", },  -- Kidney Shot
 	[6770] = { type = "cc", },  -- Sap
 	[1330] = { type = "cc", },  -- Garrote - Silence
 	[1833] = { type = "cc", },  -- Cheap Shot
@@ -233,16 +232,15 @@ BigDebuffs.Spells = {
 	-- Warlock
 	[47241] = { type = "buffs_offensive", }, -- Metamorphosis
 	[18708] = { type = "buffs_other", },  -- Fel Domination
-	[47986] = { type = "buffs_other", }, -- Sacrifice
+	[7812] = { type = "buffs_other", }, -- Sacrifice
 	[60995] = { type = "cc", }, -- Demon Charge (Metamorphosis)
-	[47847] = { type = "cc", },  -- Shadowfury
-	[30283] = { type = "cc", },
+	[30283] = { type = "cc", },  -- Shadowfury
 	[31117] = { type = "cc", },  -- Unstable Affliction (Silence)
-	[18647] = { type = "cc", },  -- Banish
-	[47860] = { type = "cc", },  -- Death Coil
+	[710] = { type = "cc", },  -- Banish
+	[47541] = { type = "cc", },  -- Death Coil
 	[6358] = { type = "cc", },  -- Seduction
-	[6215] = { type = "cc", },  -- Fear
-	[17928] = { type = "cc", },  -- Howl of Terror
+	[5782] = { type = "cc", },  -- Fear
+	[5484] = { type = "cc", },  -- Howl of Terror
 	[24259] = { type = "cc", }, -- Spell Lock (Silence)
 	[89766] = { type = "cc", }, -- Axe Toss (Felguard)
 	[19647] = { type = "interrupts", interruptduration = 6, },  -- Spell Lock (Interrupt)
@@ -262,7 +260,6 @@ BigDebuffs.Spells = {
 	[2458] = { type = "buffs_other", }, -- Berserker Stance
 	[71] = { type = "buffs_other", }, -- Defensive Stance
 	[12809] = { type = "cc", }, -- Concussion Blow
-	[12798] = { type = "cc", }, -- Revenge Stun
 	[676] = { type = "cc", },  -- Disarm
 	[46968] = { type = "cc", },  -- Shockwave
 	[5246] = { type = "cc", },  -- Intimidating Shout (Non - Target)
